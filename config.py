d8aa0b12daa9b632ff2499917ee1874312f468b3
nikita18042013-cloud <nikita18042013@gmail.com>
2025-12-24 12:53:10 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+import os
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+PDF_URL = "https://www.poe.pl.ua/disconnection/hrafik-pohodynnoho-vidkliuchennia-elektroenerhii/"
+DATA_FILE = "data.json"
